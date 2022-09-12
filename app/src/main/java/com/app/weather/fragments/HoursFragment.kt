75c09dda789b9b9f43e440533c611c6947cfe033
XArfestX <Arfest2001@gmail.com>
2022-09-12 20:55:52 +0300

    Upgraded HoursFragment.kt -> Add initRcView                              Upgraded onViewCreated Upgraded WeatherAdapter.kt -> Add class Comparator Upgraded MainFragment.kt -> Add fun requestWeatherData

--- app/src/main/java/com/app/weather/fragments/HoursFragment.kt
+++ app/src/main/java/com/app/weather/fragments/HoursFragment.kt
@@ -5,17 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.weather.R
+import com.app.weather.adapters.WeatherAdapter
+import com.app.weather.adapters.WeatherModel
+import com.app.weather.databinding.FragmentHoursBinding
 
 class HoursFragment : Fragment() {
-
-
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding){
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+        val list = listOf(
+            WeatherModel("", "12.00", "Sunny", "25°C","", "", "", ""),
+            WeatherModel("", "13.00", "Cloud", "20°C","", "", "", ""),
+            WeatherModel("", "15.00", "sunny", "27°C","", "", "", "")
+        )
+        adapter.submitList(list)
     }
 
     companion object {
--- app/src/main/java/com/app/weather/fragments/MainFragment.kt
+++ app/src/main/java/com/app/weather/fragments/MainFragment.kt
@@ -2,6 +2,7 @@ package com.app.weather.fragments
 
 import android.Manifest
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,10 +11,14 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.app.weather.R
 import com.app.weather.adapters.VpAdapter
 import com.app.weather.databinding.FragmentMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
+const val API_KEY = "7c6d391f52a34bdbb15175839221009"
 
 class MainFragment : Fragment() {
     private val fList = listOf(
@@ -39,6 +44,7 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
         init()
+        requestWeatherData("London")
     }
 
     private fun init() = with(binding){
@@ -56,12 +62,34 @@ class MainFragment : Fragment() {
 
         }
     }
-    //Проверка разрешений
+
     private fun checkPermission(){
         if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
             permissionListener()
             pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
+    }
+
+    private fun requestWeatherData(city: String){
+        val url = "https://api.weatherapi.com/v1/forecast.json?key=" +
+                API_KEY +
+                "&q=" +
+                city +
+                "&days=" +
+                "3" +
+                "&aqi=no&alerts=no"
+        val queue = Volley.newRequestQueue(context)
+        val request = StringRequest(
+            Request.Method.GET,
+            url,
+            {
+                result -> Log.d("MyLog","Result: $result")
+            },
+            {
+                error -> Log.d("MyLog","Error: $error")
+            }
+        )
+        queue.add(request)
     }
 
     companion object {
