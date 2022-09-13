f8cc06609bd2f814eefcdf34daef2f70d7a8d1eb
XArfestX <Arfest2001@gmail.com>
2022-09-13 14:26:25 +0300

    Upgraded MainFragment.kt ->      Add fun updateCurrentCard      Add fun parseWeatherData      Add fun parseDays      Add fun parseCurrentData Updated build.gradle ->      from implementation 'com.squareup.picasso:picasso:2.5.2'      to implementation 'com.squareup.picasso:picasso:2.8'

--- app/src/main/java/com/app/weather/fragments/MainFragment.kt
+++ app/src/main/java/com/app/weather/fragments/MainFragment.kt
@@ -11,13 +11,19 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.activityViewModels
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.app.weather.MainViewModel
 import com.app.weather.R
 import com.app.weather.adapters.VpAdapter
+import com.app.weather.adapters.WeatherModel
 import com.app.weather.databinding.FragmentMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
+import com.squareup.picasso.Picasso
+import org.json.JSONObject
+
 const val API_KEY = "7c6d391f52a34bdbb15175839221009"
 
 class MainFragment : Fragment() {
@@ -31,7 +37,7 @@ class MainFragment : Fragment() {
     )
     private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
-
+    private val model: MainViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -44,6 +50,7 @@ class MainFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
         init()
+        updateCurrentCard()
         requestWeatherData("London")
     }
 
@@ -53,6 +60,19 @@ class MainFragment : Fragment() {
         TabLayoutMediator(tabLayout, vp){
             tab, pos -> tab.text = tList[pos]
         }.attach()
+    }
+
+    private fun updateCurrentCard() = with(binding){
+        model.liveDataCurrent.observe(viewLifecycleOwner){
+            val maxMinTemp = "${it.maxTemp}°C/${it.minTemp}°C"
+            val currentTemp = "${it.currentTemp}°C"
+            tvData.text = it.time
+            tvCity.text = it.city
+            tvCurrentTemp.text = currentTemp
+            tvCondition.text = it.condition
+            tvMaxMin.text = maxMinTemp
+            Picasso.get().load("https:" + it.imageUrl).into(imWeather)
+        }
     }
 
     private fun permissionListener(){
@@ -83,13 +103,63 @@ class MainFragment : Fragment() {
             Request.Method.GET,
             url,
             {
-                result -> Log.d("MyLog","Result: $result")
+                result -> parseWeatherData(result)
             },
             {
                 error -> Log.d("MyLog","Error: $error")
             }
         )
         queue.add(request)
+    }
+
+    private fun parseWeatherData(result: String){
+        val mainObject = JSONObject(result)
+        val list = parseDays(mainObject)
+        parseCurrentData(mainObject, list[0])
+    }
+
+    private fun parseDays(mainObject: JSONObject): List<WeatherModel>{
+        val list = ArrayList<WeatherModel>()
+        val daysArray = mainObject.getJSONObject("forecast")
+            .getJSONArray("forecastday")
+        val name = mainObject.getJSONObject("location").getString("name")
+        for (index in 0 until daysArray.length()){
+            val day = daysArray[index] as JSONObject
+            val item = WeatherModel(
+                name,
+                day.getString("day"),
+                day.getJSONObject("day")
+                    .getJSONObject("condition").getString("text"),
+                "",
+                day.getJSONObject("day").getString("maxtemp_c"),
+                day.getJSONObject("day").getString("mintemp_c"),
+                day.getJSONObject("day")
+                    .getJSONObject("condition").getString("icon"),
+                day.getJSONArray("hour").toString()
+            )
+            list.add(item)
+        }
+        return list
+    }
+
+    private fun parseCurrentData(mainObject: JSONObject, weatherItem: WeatherModel){
+
+        val item = WeatherModel(
+            mainObject.getJSONObject("location").getString("name"),
+            mainObject.getJSONObject("current").getString("last_updated"),
+            mainObject.getJSONObject("current")
+                .getJSONObject("condition").getString("text"),
+            mainObject.getJSONObject("current").getString("temp_c"),
+            weatherItem.maxTemp,
+            weatherItem.minTemp,
+            mainObject.getJSONObject("current")
+                .getJSONObject("condition").getString("icon"),
+            weatherItem.hours
+        )
+        model.liveDataCurrent.value = item
+        Log.d("MyLog","CurrentTemp: = ${item.currentTemp}")
+        Log.d("MyLog","MaxTemp: = ${item.maxTemp}")
+
     }
 
     companion object {
