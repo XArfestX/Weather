bb7216b12a62c70d27b4b381d1a5e617a575e91d
XArfestX <Arfest2001@gmail.com>
2022-09-15 16:36:45 +0300

    Upgraded DialogManager.kt ->     Add fun searchByNameDialog
    Upgraded MainFragment.kt ->
        Edited fun init
    
    Changed app icon

--- app/src/main/java/com/app/weather/DialogManager.kt
+++ app/src/main/java/com/app/weather/DialogManager.kt
@@ -2,6 +2,7 @@ package com.app.weather
 
 import android.app.AlertDialog
 import android.content.Context
+import android.widget.EditText
 
 object DialogManager {
     fun locationSettingsDialog(context: Context, listener: Listener){
@@ -10,7 +11,23 @@ object DialogManager {
         dialog.setTitle("Enable location?")
         dialog.setMessage("Location disable, do you want enable location?")
         dialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK"){ _,_ ->
-            listener.onClick()
+            listener.onClick(null)
+            dialog.dismiss()
+        }
+        dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel"){ _,_ ->
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    fun searchByNameDialog(context: Context, listener: Listener){
+        val builder = AlertDialog.Builder(context)
+        val editName = EditText(context)
+        builder.setView(editName)
+        val dialog = builder.create()
+        dialog.setTitle("City name:")
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK"){ _,_ ->
+            listener.onClick(editName.text.toString())
             dialog.dismiss()
         }
         dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel"){ _,_ ->
@@ -20,6 +37,6 @@ object DialogManager {
     }
 
     interface Listener{
-        fun onClick()
+        fun onClick(name: String?)
     }
 }
